2b4925167f0f9c41ca6a3e279bedf56e02fce1a8
Erlangga Deanda CS <erlangzdcs@gmail.com>
2026-05-04 12:50:34 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/pnm/yourtexttoanonymous/MainActivity.kt
@@ -0,0 +1,51 @@
+package id.ac.pnm.yourtexttoanonymous
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import id.ac.pnm.yourtexttoanonymous.ui.theme.YourTextToAnonymousTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            YourTextToAnonymousTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.fillMaxHeight(),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    YourTextToAnonymousTheme {
+        Greeting("Android")
+    }
+}
